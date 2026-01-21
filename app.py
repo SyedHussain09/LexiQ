145d92a346e4b1a7e2a6df945282c87c76409e29
Syed Sajjad Hussain <syedsajjadhussain014@gmail.com>
2026-01-21 18:26:41 +0530

    fixing deployment issue

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 
 from __future__ import annotations
 
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 import streamlit as st
 
 from uae_legal_rag.app_ui import render_app
